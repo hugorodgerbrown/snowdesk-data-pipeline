f41ec73a5d3fc42304455f5b7e3df5680bb4bb80
Hugo Rodger-Brown <hugo@rodger-brown.com>
2026-04-11 18:11:06 +0100

    Add more EAWS icons

--- public/templatetags/hazard_icons.py
+++ public/templatetags/hazard_icons.py
@@ -5,23 +5,21 @@ Maps CAAML avalanche problem types to their EAWS SVG icon paths under
 ``static/icons/svg/``.
 """
 
+from os import path
+
 from django import template
 
 register = template.Library()
 
 # Mapping from CAAML problemType values to SVG filenames in static/icons/svg/.
 _ICON_MAP: dict[str, str] = {
-    "gliding_snow": "icons/svg/Icon-Avalanche-Problem-Gliding-Snow-Grey-EAWS.svg",
-    "new_snow": "icons/svg/Icon-Avalanche-Problem-New-Snow-Grey-EAWS.svg",
-    "persistent_weak_layers": (
-        "icons/svg/Icon-Avalanche-Problem-Persistent-Weak-Layer-Grey-EAWS.svg"
-    ),
-    "wet_snow": "icons/svg/Icon-Avalanche-Problem-Wet-Snow-Grey-EAWS.svg",
-    "wind_slab": "icons/svg/Icon-Avalanche-Problem-Wind-Slab-Grey-EAWS.svg",
-    "no_distinct_avalanche_problem": (
-        "icons/svg/Icon-Avalanche-Problem-No-Distinct-Avalanche-Problem-EAWS.svg"
-    ),
-    "cornices": "icons/svg/Icon-Avalanche-Problem-Cornices.svg",
+    "gliding_snow": "Gliding-Snow.svg",
+    "new_snow": "New-Snow.svg",
+    "persistent_weak_layers": "Persistent-Weak-Layer.svg",
+    "wet_snow": "Wet-Snow.svg",
+    "wind_slab": "Wind-Slab.svg",
+    "no_distinct_avalanche_problem": "No-Distinct-Avalanche-Problem.svg",
+    "cornices": "Cornices.svg",
 }
 
 
@@ -41,4 +39,4 @@ def hazard_icon(problem_type: str) -> str:
         The icon path relative to STATIC_URL, or empty string if unknown.
 
     """
-    return _ICON_MAP.get(problem_type, "")
+    return path.join("icons/eaws/avalanche_problems/", _ICON_MAP.get(problem_type, ""))
